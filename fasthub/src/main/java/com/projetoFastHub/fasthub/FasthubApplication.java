88babe49e9f27866e29e57585ed2c6a0e73f5e55
jclaudio <jv.claudio@bol.com.br>
2025-03-14 21:25:37 -0300

    Criando pacote de servico

--- fasthub/src/main/java/com/projetoFastHub/fasthub/FasthubApplication.java
+++ fasthub/src/main/java/com/projetoFastHub/fasthub/FasthubApplication.java
@@ -2,12 +2,14 @@ package com.projetoFastHub.fasthub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
-public class FasthubApplication {
+@Theme("my-theme")
+public class FasthubApplication implements AppShellConfigurator {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FasthubApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FasthubApplication.class, args);
+    }
 }
---
+++ fasthub/src/main/java/com/projetoFastHub/fasthub/servico/ServicoModel.java
@@ -0,0 +1,4 @@
+package com.projetoFastHub.fasthub.servico;
+
+public class ServicoModel {
+}
