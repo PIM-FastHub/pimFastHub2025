65ccddd8409c732b421eb456fb2949c5c6188eca
jclaudio <jv.claudio@bol.com.br>
2025-03-14 21:07:40 -0300

    Add estrutura inicial do projeto

---
+++ fasthub/src/main/java/com/projetoFastHub/fasthub/FasthubApplication.java
@@ -0,0 +1,13 @@
+package com.projetoFastHub.fasthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasthubApplication.class, args);
+	}
+
+}
---
+++ fasthub/src/test/java/com/projetoFastHub/fasthub/FasthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoFastHub.fasthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
